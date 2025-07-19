cbe513317a23637d4af2467e1e8715b5e2b7bf15
Romina Dehesa <rominadehesa@gmail.com>
2025-07-19 15:39:32 -0300

    add section'Tradizione che ispira innovazione'

---
+++ src/components/ImageRightSection.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let image = '';
+  export let title = '';
+  export let text = '';
+</script>
+
+<div class="container">
+    <div>
+        <h2>{title}</h2>
+        <br>
+        <p>{text}</p>
+        <slot></slot>
+    </div>
+    <img src={image} alt="Image Right Section">
+</div>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .container div {
+        max-width: 40%;
+    }
+
+    h2 {
+        text-align: left;
+    }
+
+    img {
+        max-width: 40%;
+        height: auto;
+    }
+
+    p{
+        font-size: 16px;    
+        line-height: 38px;
+        letter-spacing: 0%;
+        color: #5B5B5B;
+    }
+</style>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,18 +1,24 @@
 <script>
     import Hero from '../components/Hero.svelte';
     import ContainerGrey from '../components/container-grey.svelte';
+    import ImageRightSection from '../components/ImageRightSection.svelte';
     const backgroundImage = '/hero1.webp';
+    const imageRightSection1 = '/home1.png';
 </script>
 
 <Hero backgroundImage={backgroundImage}>
     <h1>Innovando forni <br> professionali <span>dal 1980</span></h1>
 </Hero>
 
-<ContainerGrey ifSubitutle={'Made in italy'} ifTitle={'Qualità, prestazioni ed efficienza'}>
-    <div>
-        <p>Fondata negli anni '80 a Cremona, siamo stati pionieri nel mondo dei forni combinati a vapore. Nel 2019, il nostro viaggio si è evoluto con un nuovo capitolo a Torino, guidato dalla nostra passione per l'eccellenza tecnologica e la ricerca delle condizioni di cottura perfette. Ogni forno che costruiamo viene attentamente testato dai nostri tecnici esperti durante il processo di assemblaggio, garantendo affidabilità e precisione. Il nostro impegno è creare forni che siano intuitivi, durevoli e che offrano prestazioni eccezionali.</p>
-    </div>
+<ContainerGrey ifSubitutle={'Made in italy'} ifTitle={'Qualità, prestazioni ed efficienza'} text={"Fondata negli anni '80 a Cremona, siamo stati pionieri nel mondo dei forni combinati a vapore. Nel 2019, il nostro viaggio si è evoluto con un nuovo capitolo a Torino, guidato dalla nostra passione per l'eccellenza tecnologica e la ricerca delle condizioni di cottura perfette. Ogni forno che costruiamo viene attentamente testato dai nostri tecnici esperti durante il processo di assemblaggio, garantendo affidabilità e precisione. Il nostro impegno è creare forni che siano intuitivi, durevoli e che offrano prestazioni eccezionali."}>
 </ContainerGrey>
+
+<section class="padding-lr padding-bt">
+    <ImageRightSection image={imageRightSection1} title={'Tradizione che ispira innovazione'} text={'Da oltre 40 anni perfezioniamo l’eccellenza culinaria. Con passione tutta italiana, uniamo tradizione e innovazione per offrire soluzioni uniche ai professionisti della cucina.'}>
+        <br>
+        <button id="btnPrimary-black">Scopri di più</button>
+    </ImageRightSection>
+</section>
 
 <style>
 
@@ -37,11 +43,7 @@
         letter-spacing: -2%;
         vertical-align: middle;
     }
-    .container-grey {
-        background-color: #F5F5F5;
-        padding: 10vh 15vh;
-        text-align: center;
-    }
+
     div {
        display: flex;
        justify-content: center;
