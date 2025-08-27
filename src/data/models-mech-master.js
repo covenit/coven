c74ac6d44583d1f0409f41cc2a350066efc7925a
Romina Dehesa <rominadehesa@gmail.com>
2025-08-27 13:33:15 -0300

    FIX

---
+++ src/data/models-mech-master.js
@@ -0,0 +1,20 @@
+export const models = [
+    {
+        model: "N05EMDMH - 5 TEGLIE",
+        text: "Forno combinato a vapore ad alta efficienza",
+        line: "MASTER DELIGHT MECH",
+        isElec: true,
+        typeElec: "Elettrico",
+        isGas: false,
+        typeGas: "Gas",
+        urlImg: "/models/steady-cook/merch/N05EMDMH.webp",
+        dimesterne: "862x716x728 mm",
+        diminterne: "584x355x436 mm",
+        numeroteglie: "6 - 1/1 GN",
+        distanzateglie: "60 mm",
+        numeropasti: "max 70",
+        peso: "91 Kg",
+        tensione: "415 ~ 3N",
+        potenza: "7,8 kW"
+    },
+];
