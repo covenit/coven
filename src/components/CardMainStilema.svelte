5672364fa4bca02989154c7a0d0dc51e70ab0f20
Romina Dehesa <rominadehesa@gmail.com>
2025-08-29 16:54:36 -0300

    fix

---
+++ src/components/CardMainStilema.svelte
@@ -0,0 +1,100 @@
+<script>
+    export let title;
+    export let subtitle;
+    export let text;
+    export let urlImg;
+
+    let showInfo = false;
+</script>
+
+<article style="--img: url({urlImg})" class:active={showInfo}>
+    {#if !showInfo}
+        <div>
+            <h4>{title}</h4>
+            <p>{subtitle}</p>
+            <button on:click={() => showInfo = true}>
+                Scopri di più
+                <svg width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M15.1872 9.02793C14.9866 9.23214 14.7148 9.34684 14.4314 9.34684C14.1479 9.34684 13.8761 9.23214 13.6755 9.02793L8.01381 3.25571L2.35211 9.02793C2.25419 9.13506 2.13611 9.221 2.00491 9.2806C1.87371 9.3402 1.73207 9.37225 1.58846 9.37483C1.44484 9.37741 1.30219 9.35048 1.16901 9.29563C1.03583 9.24079 0.914842 9.15916 0.813275 9.05561C0.711709 8.95206 0.63164 8.82871 0.577845 8.69293C0.524051 8.55715 0.497632 8.41171 0.500166 8.26529C0.5027 8.11888 0.534135 7.97448 0.592594 7.84071C0.651054 7.70695 0.73534 7.58656 0.840427 7.48673L7.25797 0.943912C7.45852 0.739702 7.73037 0.625 8.01381 0.625C8.29725 0.625 8.56911 0.739702 8.76965 0.943912L15.1872 7.48673C15.3875 7.69119 15.5 7.96835 15.5 8.25733C15.5 8.5463 15.3875 8.82346 15.1872 9.02793Z" fill="white"/>
+                </svg>
+            </button>
+        </div>
+    {:else}
+        <div>
+            <p class="text-info">{text}</p>
+            <button on:click={() => showInfo = false}>Chiudi</button>
+        </div>
+    {/if}
+</article>
+
+<style>
+    article {
+        background-image:
+            linear-gradient(to bottom, rgba(0,0,0,0.1) 50%, rgba(0,0,0,0.7) 70%, rgba(0,0,0,0.9) 100%),
+            var(--img);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        width: 49%;
+        height: 470px;
+        border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: end;
+        text-align: center;
+        padding: 5%;
+        overflow: hidden;
+        transition: all 0.3s ease-in-out;
+    }
+
+    article.active {
+         background-image:
+            linear-gradient(to bottom, rgba(0,0,0,0.1) 0%, rgba(0,0,0,0.6) 40%, rgba(0,0,0,0.8) 100%),
+            var(--img);
+    }
+
+    article div h4 {
+        font-family: 'sf-ui-display-heavy';
+        font-size: 24px;
+        line-height: 24px;
+        color: #FFFFFF;
+        margin-bottom: 10px;
+    }
+
+    article div p {
+        font-family: 'sf-ui-display-regular';
+        font-size: 18px;
+        line-height: 32px;
+        color: #FFFFFF;
+        margin-bottom: 10px;
+    }
+
+    article div button {
+        background: none;
+        border: none;
+        cursor: pointer;
+        font-family: 'sf-ui-display-bold';
+        font-size: 14px;
+        line-height: 100%;
+        color: #FFFFFF;
+    }
+
+    article div button svg {
+        margin-left: 5px;
+    }
+
+    article .text-info {
+        font-size: 18px;
+        line-height: 28px;
+        text-align: start;
+    }
+
+    @media (max-width: 767px) {
+        article {
+            width: 100%;
+            margin-bottom: 2vh;
+            height: 220px;
+            border-radius: 10px;
+        }
+    }
+</style>
--- src/routes/Styleme.svelte
+++ src/routes/Styleme.svelte
@@ -8,6 +8,7 @@
     import CustomizedTools from "../components/CustomizedTools.svelte";
     import SectionModels from "../components/SectionModels.svelte";
     import { models } from "../data/models-stilema";
+    import CardMainStilema from "../components/CardMainStilema.svelte";
 
     const backgroundImage = '/hero-stilema.webp';
     const image = "/line/stilema.webp";
@@ -56,86 +57,23 @@
     <Carrousel images={images}></Carrousel>
 </section>
 
-
-<section class="padding-lr padding-bt section-information" style="background-color: #F8F8F8;">
-    <div class="main-info">
-        <h4>CAMERA DI COTTURA</h4>
-        <h5>Uniformità assoluta. Il futuro non lascia punti freddi.</h5>
-        <br>
-        <p>
-            Realizzata completamente in acciaio inox, la camera di cottura di STILEMA consente il raggiungimento e il mantenimento della temperatura fino a 300°. Grazie al KNOW HOW del nostro comparto tecnico, ai continui studi e test di laboratorio, abbiamo raggiunto la massima efficienza e precisione della distribuzione dell’aria calda e la conseguente totale uniformità della temperatura. I risultati di cottura saranno sempre impeccabili in ogni singolo spazio utilizzato.
-            STILEMA garantisce massima flessibilità in cucina, con capacità di carico teglie GN1/1 e 60X40 per garantire i migliori risultati sia per la Gastronomia che per la Pasticceria.</p>
-    </div>
-    <img src="/carrousel-stilema-line/A.webp" alt="">
-</section>
-
-<section class="padding-lr padding-bt section-back-information" style="background-image: url('/carrousel-stilema-line/background-a.webp');">
-    <h4>SWIRLING GAS SYSTEM </h4>
-    <h5>La rivoluzione del calore intelligente</h5>
+<section class="padding-lr padding-bt cards-stilema">
+    <h2>L’eccellenza di Stilema, in ogni dettaglio</h2>
     <br>
-    <div>
-        <p>
-           Stilema utilizza un bruciatore brevettato con tecnologia SWIRLING GAS SYSTEM, che grazie alla miscelazione non premix e al controllo elettronico, raggiunge rapidamente temperature fino a 300 °C garantendo cotture perfette anche a pieno carico.Questa tecnologia assicura alte prestazioni con basse emissioni, risparmio energetico e massima sicurezza, grazie alla modulazione P.I.D. e alla gestione precisa del rapporto aria/gas.
-        </p>
-    </div>
-</section>
+    <main>
+        <CardMainStilema urlImg={'/carrousel-stilema-line/aa.png'} title="CAMERA DI COTTURA" subtitle="Uniformità assoluta. Il futuro non lascia punti freddi" 
+            text="Realizzata completamente in acciaio inox, la camera di cottura di STILEMA consente il raggiungimento e il mantenimento della temperatura fino a 300°. Grazie al KNOW HOW del nostro comparto tecnico, ai continui studi e test di laboratorio, abbiamo raggiunto la massima efficienza e precisione della distribuzione dell’aria calda e la conseguente totale uniformità della temperatura. I risultati di cottura saranno sempre impeccabili in ogni singolo spazio utilizzato.
+            STILEMA garantisce massima flessibilità in cucina, con capacità di carico teglie GN1/1 e 60X40 per garantire i migliori risultati sia per la Gastronomia che per la Pasticceria."
+        >
+        </CardMainStilema>
 
-<section class="padding-lr padding-bt section-information">
-    <img src="/carrousel-stilema-line/B.webp" alt="">
-    
-    <div class="main-info">
-        <h4>AIR FLOW</h4>
-        <h5>La nuova era della flessibilità è nell’aria.</h5>
-        <br>
-        <p>La ventola di STILEMA, comandata da un motore brushless e relativo inverter, è in grado di variare automaticamente il senso di rotazione e la velocità in funzione dei diversi processi di cottura e del calore richiesto per ogni preparazione. Ciò garantisce massima flessibilità ed azione garantendo una velocità di cottura e di precisione senza precedenti. <br>
-La scelta di utilizzare componentistica made in Italy di alta qualità, progettata e realizzata in partnership con i nostri fornitori, garantisce a STILEMA massima robustezza e performance ottimali oltre a semplificare gli interventi di manutenzione.</p>
-    </div>
+        <CardMainStilema urlImg={'/carrousel-stilema-line/aa.png'} title="CAMERA DI COTTURA" subtitle="Uniformità assoluta. Il futuro non lascia punti freddi" 
+            text="Realizzata completamente in acciaio inox, la camera di cottura di STILEMA consente il raggiungimento e il mantenimento della temperatura fino a 300°. Grazie al KNOW HOW del nostro comparto tecnico, ai continui studi e test di laboratorio, abbiamo raggiunto la massima efficienza e precisione della distribuzione dell’aria calda e la conseguente totale uniformità della temperatura. I risultati di cottura saranno sempre impeccabili in ogni singolo spazio utilizzato.
+            STILEMA garantisce massima flessibilità in cucina, con capacità di carico teglie GN1/1 e 60X40 per garantire i migliori risultati sia per la Gastronomia che per la Pasticceria."
+        >
+        </CardMainStilema>
+    </main>
 </section>
-
-<section class="padding-lr padding-bt section-back-information" style="background-image: url('/carrousel-stilema-line/background-b.webp');">
-    <h4>DUO STEAM SYSTEM (VAPORE)</h4>
-    <h5>Controllo assoluto, efficienza che guarda al domani.</h5>
-    <br>
-    <div>
-        <p>
-            Innovativo sistema combinato che produce vapore consentendo di regolare la percentuale di umidità. Una caldaia dedicata conduce vapore nella camera di cottura insieme a dispositivi che ne consentono la produzione diretta in un binomio perfetto di efficienza. <br>
-            DUO STEAM SYSTEM permette di realizzare cotture rapide ad alta temperatura per esaltare la croccantezza degli alimenti e preparazioni lente a bassa temperatura per preservare le proprietà organolettiche. STILEMA permette di cuocere alimenti sottovuoto, con minori perdite di peso e una maggiore esaltazione dei sapori ed è dotata di un inverter che permette di controllare le ventole a 9 differenti velocità.
-        </p>
-    </div>
-</section>
-
-<section class="padding-lr padding-bt section-information carrousel-container">
-    <img src="/line/stilema.webp" alt="">
-    
-    <div class="main-info">
-        <h4>SWIRLING GAS SYSTEM</h4>
-        <h5>La rivoluzione del calore intelligente</h5>
-        <br>
-        <div>
-            <p>
-                L’innovativo sistema di Lavaggio Automatico Professionale, si prende cura di Stilema mantenendo una pulizia sempre accurata ed efficace e garantendo elevati livelli di igiene per l’ambiente professionale.
-                Sono disponibili 4 differenti programmi di lavaggio per adattarsi sempre di più ad ogni necessità: solo risciacquo, lavaggio delicato, medio e intenso. <br>
-                Il riutilizzo dell’acqua calda garantisce un fattivo risparmio idrico ed energetico così come l’utilizzo di detergenti biologici e di brillantanti naturali garantiscono la giusta attenzione alla sostenibilità.
-            </p>
-        </div>
-    </div>
-</section>
-
-<section class="padding-lr padding-bt section-information">
-    
-    <div class="main-info">
-        <h4>SEMPRE CONNESSO</h4>
-        <h5>La cucina del futuro, a portata di click.</h5>
-        <br>
-        <p>
-            L’interfaccia Wi-Fi consente a STILEMA di connettersi facilmente a internet per segnalare guasti, programmare interventi di manutenzione e scaricare i dati HACCP in modo immediato. <br>
-            Grazie alla connettività cloud, è possibile accedere da remoto in tempo reale tramite PC, monitorare tutti i parametri funzionali e aggiornare i programmi di cottura, ottimizzando la gestione del lavoro in cucina. Inoltre, la porta USB permette di salvare dati e aggiornare il software in modo semplice e intuitivo.
-        </p>
-    </div>
-    
-    <img src="/carrousel-stilema-line/C.webp" alt="">
-</section>
-
 
 <section class="padding-lr padding-bt carrousel-container section-details">
     <header>
@@ -174,60 +112,6 @@ La scelta di utilizzare componentistica made in Italy di alta qualità, progetta
 <Form complete={false}></Form>
 
 <style>
-    .section-information {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    .section-information img, .section-information .main-info{
-        width: 45%;
-    }
-
-    .section-information .main-info h4, .section-information .main-info h5,   .section-back-information h5,   .section-back-information h4 {
-        font-family: "sf-ui-display-bold";
-        font-size: 18px;    
-        line-height: 46px;
-        letter-spacing: -2%;
-        color: #2F2F2F;
-    }
-    
-    .section-information .main-info h4,   .section-back-information h4 {
-        font-size: 20px; 
-    }
-
-    .section-information p,   .section-back-information p{
-        font-family: "sf-ui-display-regular";
-        font-size: 18px;
-        line-height: 38px;
-        letter-spacing: 0%;
-        color: #5B5B5B;
-    }
-    
-    .section-back-information {
-        background-size: cover;
-        background-position: center center;
-        background-color: rgba(0, 0, 0, 0.6); /* negro semitransparente arriba */
-        background-blend-mode: darken; /* o multiply, overlay, etc. */
-    }
-    .section-back-information h4, .section-back-information h5{
-        text-align: center;
-        color: white;
-    }
-    .section-back-information > div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .section-back-information p {
-        width: 50%;
-        text-align: center;
-        color: white;
-    }
-    .section-back-information h4 {
-        font-size: 24px;
-        line-height: 52px;
-        letter-spacing: -2%;
-    }
     .section-details header h5 {
         font-family: "sf-ui-display-regular";
         font-size: 20px;
@@ -270,24 +154,15 @@ La scelta di utilizzare componentistica made in Italy di alta qualità, progetta
         margin: 2vh 0;
     }
 
+    .cards-stilema main {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+        gap: 1vw;
+    }
+
     @media (max-width: 767px) {
-        .section-information {
-            display: block;
-        }
-        .section-information img, .section-information .main-info {
-            width: 100%;
-            margin-bottom: 20px;
-        }
-        .section-back-information p {
-            width: 100%;
-             text-align: start;
-            color: white;
-        }
-        .section-information .main-info h5,   .section-back-information h5 {
-            font-size: 16px; 
-            font-family: "sf-ui-display-medium";
-            text-align: start;
-        }
         .section-details main {
             display: block;
         }
@@ -307,6 +182,9 @@ La scelta di utilizzare componentistica made in Italy di alta qualità, progetta
         .section-details header h5 {
             font-size: 16px;
             text-align: start;
+        }
+        .cards-stilema main { 
+            display: block;
         }
     }
 
