275b81b948af06e59f9301e8a9f8b8fae6063738
Romina Dehesa <rominadehesa@gmail.com>
2025-10-28 10:30:30 -0300

    fix

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,14 +1,40 @@
 <script>
-  const banner1 = "/banner1.webp";
-  const banner2 = "/banner2.webp";
+  document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".slide");
+    let current = 0;
+
+    // Oculta todos los slides excepto el primero
+    slides.forEach((slide, index) => {
+      // @ts-ignore
+      slide.style.display = index === 0 ? "flex" : "none";
+    });
+
+    // Cambia de slide cada 5 segundos
+    setInterval(() => {
+      // @ts-ignore
+      slides[current].style.display = "none";
+      current = (current + 1) % slides.length;
+      // @ts-ignore
+      slides[current].style.display = "flex";
+    }, 5000);
+  });
 </script>
 
 <header class="hero">
-  <!--Banner 1 --> 
-  <div class="slide slide1 padding-lr">
+  <!--Banner 1--> 
+  <div class="slide padding-lr slide1">
     <h1>
       Passione made in Italy <br> per i forni professionali
       <p>dal 1980</p>
+    </h1>
+  </div>
+
+  <!--Banner 2--> 
+ <div class="slide padding-lr slide2">
+    <h1>
+      Host Milano 2025
+      <br>
+      <p><a href="https://www.instagram.com/p/DQJx8ZmkdZ1/?img_index=1">Scopri di più</a></p>
     </h1>
   </div>
 </header>
@@ -18,14 +44,30 @@
   position: relative;
   width: 100%;
   height: 70vh;
+  overflow: hidden;
   display: flex;
-  justify-content: center;
-  align-items: center;
+}
+
+.slide {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 70vh;
   background-size: cover;
   background-position: center;
+  justify-content: center;
+  align-items: center;
+  display: none;
+}
+
+.slide1 {
   background-image: url("./../../public/hero1.webp");
 }
 
+.slide2 {
+  background-image: url("./../../public/home2.webp");
+}
 
 /* Texto */
 h1 {
@@ -40,6 +82,11 @@ h1 p {
   color: #e20c18;
   font-size: 54px;
   margin: 0;
+}
+p a {
+  text-decoration: none;
+  color: #e20c18;
+  font-size: 40px;
 }
 
 /* Responsive */
